733174c9991551946f3494fa0650a88179acdeb8
Pascal Honegger <pascal.honegger.external@atos.net>
2017-11-03 08:48:40 +0100

    Backend-Projekt erstellt

---
+++ AtosHappyMeter/AtosHappyMeter/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace AtosHappyMeter
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+			var enableForAllAttribute = new EnableCorsAttribute("*", "*", "*");
+			config.EnableCors(enableForAllAttribute);
+
+			// Web API routes
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				"DefaultApi",
+				"api/{controller}/{action}",
+				new {controller = "home", action = "index"}
+			);
+		}
+	}
+}
