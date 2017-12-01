3e0c7ddf152349eec0f29e6ac56b55f57082daa5
Pascal Honegger <pascal.honegger.external@atos.net>
2017-12-01 16:16:15 +0100

    Fix prod server path & AOT-Build

--- Frontend/src/app/services/server.service.ts
+++ Frontend/src/app/services/server.service.ts
@@ -2,9 +2,10 @@ import { isDevMode } from '@angular/core';
 
 const baseUrlDebug: string = 'http://localhost:49484/api/';
 
-// Used for production release
+// Used for testing
 // const baseUrlProd: string = 'http://atoshappymeter.azurewebsites.net/api/';
-const baseUrlProd: string = 'http://ahmlatest.azurewebsites.net/api/';
+// const baseUrlProd: string = 'http://ahmlatest.azurewebsites.net/api/';
+const baseUrlProd: string = '../api/';
 
 export abstract class ServerService {
     protected readonly baseUrl: string;
