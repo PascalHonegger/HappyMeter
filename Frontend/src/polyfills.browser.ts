ffd0fedd2094f8ea68dbde1d6fd56f03f9263cbf
Pascal Honegger <pascal.honegger.external@atos.net>
2017-12-01 09:34:08 +0100

    NPM Update

--- Frontend/src/polyfills.browser.ts
+++ Frontend/src/polyfills.browser.ts
@@ -18,9 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-// Internet Explorer 9 support
-// import 'ie-shim';
-
 // IE9, IE10 and IE11 requires all of the following polyfills.
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
