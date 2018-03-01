eacec0352a15e6e35143c99726387ab0946b9b63
Pascal Honegger <pascal.honegger.external@atos.net>
2018-03-01 14:20:28 +0100

    Fix delete-emoji-picker-ts.ps1

--- Frontend/delete-emoji-picker-ts.ps1
+++ Frontend/delete-emoji-picker-ts.ps1
@@ -1,8 +1,7 @@
 $TARGETDIR = 'node_modules/angular2-emoji-picker/lib-dist'
 if (Test-Path -Path $TARGETDIR){
-	cd $TARGETDIR
-	remove-item * -recurse -include *.ts -exclude *.d.ts
-	cd '../../..'
+	echo "Deleting all .ts files from $TARGETDIR"
+	get-childitem $TARGETDIR -include *.ts -exclude *.d.ts -recurse | foreach ($_) {remove-item $_.fullname}
 } else {
 	echo 'Folder not found'
 }
