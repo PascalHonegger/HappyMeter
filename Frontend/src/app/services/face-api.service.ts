e6233af06950ab5401dc0fc503f3a62f03caaa7d
Pascal Honegger <pascal.honegger.external@atos.net>
2018-03-29 15:37:52 +0200

    Small fixes & AOT-Build

--- Frontend/src/app/services/face-api.service.ts
+++ Frontend/src/app/services/face-api.service.ts
@@ -36,7 +36,7 @@ export class FaceApiService {
                     // 'blur',
                     // 'exposure',
                     // 'noise'
-                ]
+                ].join(',')
             }}),
             headers: this.headers
         });
